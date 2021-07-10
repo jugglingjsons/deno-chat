194c3288a0db3e47c90a88c4de3178cdfd3943ad
jugglingjsons <jadczak.d@gmail.com>
2021-07-10 14:56:10 +0200

    feat: oak added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { Application } from "https://deno.land/x/oak/mod.ts";
 const app = new Application();
 
 app.use((ctx) => {
-  ctx.response.body = "Hello World! from oak";
+  ctx.response.body = "Hello World! from oak!";
 });
 
 addEventListener("fetch", app.fetchEventHandler());

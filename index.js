d55734b38fe959f67019a4941eceb61e0be01a16
jugglingjsons <jadczak.d@gmail.com>
2021-07-10 14:59:52 +0200

    feat: mutliroute

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 
 const app = new Application();
 
-app.use((ctx) => {
-  ctx.response.body = "Hello World! from oak!";
-});
+const messages = [];
+
+const router = new Router();
+router
+  .get("/", (context) => {
+    context.response.body = "Basic chat server!";
+  })
+  .get("/messages", (context) => {
+    context.response.body = messages
+  })
+  .post("/book/:id", async (context) => {
+    const message = await context.request.body().value;
+    messages.push(message)
+  });
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 addEventListener("fetch", app.fetchEventHandler());
